9fe40576efaef6d24f35f06e90071baea41361f0
Douglas Pereira <douglas_s_pereira@estudante.sc.senai.br>
2022-09-11 18:19:23 -0300

    Conexão ao Banco de Dados

--- src/Principal.java
+++ src/Principal.java
@@ -6,103 +6,121 @@ import controller.TipoPesquisaController;
 import model.Candidato;
 import model.Pesquisa;
 import model.TipoPesquisa;
-import java.time.LocalDate;
+import util.ConnectionUtil;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+//import java.time.LocalDate;
 
 public class Principal {
 	
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		
-	testeCandidato();
-	testeTipoPesquisa();
-	testePesquisa();
-		
-}
-	
-	public static void testeCandidato() {
-		Candidato c1 = new Candidato();
-		c1.setId(1);
-		c1.setNome("Bolsonaro");
-		c1.setPartido("PSL");
-		c1.setFichaLimpa("Sim");
-		
-		Candidato c2 = new Candidato();
-		c2.setId(2);
-		c2.setNome("Lula");
-		c2.setPartido("PT");
-		c2.setFichaLimpa("Não");
-		
-		CandidatoController controller = new CandidatoController();
-		try {
-			controller.salvar(c1);
-			controller.salvar(c2);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		for (Candidato c : controller.listar()) {
-			System.out.println(c.toString());
-		}
-		
-		
+		consulta();
+		System.out.println("-------------------------------");
+		salvar();
+		consulta();
+		System.out.println("-------------------------------");
+			
+	//	testeCandidato();
+	//	testeTipoPesquisa();
+	//	testePesquisa();
+			
 	}
-	// comentario teste
-	public static void testeTipoPesquisa() {
-		TipoPesquisa tp1 = new TipoPesquisa();
-		tp1.setId(15);
-		tp1.setInstituto("Data Folha");
-		tp1.setDataPesquisa(LocalDate.now());
-		tp1.setLocal("Florianópolis");
-		tp1.setMediaIdade(45.5);
-		tp1.setTipoPesquisa("Prefeito");
-		tp1.setDescricao("Pesquisa para prefeito");
-		
-		TipoPesquisa tp2 = new TipoPesquisa();
-		tp2.setId(25);
-		tp2.setInstituto("Fake News");
-		tp2.setDataPesquisa(LocalDate.now());
-		tp2.setLocal("Rio de Janeiro");
-		tp2.setMediaIdade(32.7);
-		tp2.setTipoPesquisa("Presidente");
-		tp2.setDescricao("Pesquisa para presidente");
+	
 
-		
-		TipoPesquisaController controller = new TipoPesquisaController();
-		try {
-			controller.salvar(tp1);
-			controller.salvar(tp2);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		for (TipoPesquisa tp : controller.listar()) {
-			System.out.println(tp.toString());
-		}
-	}
 	
-	public static void testePesquisa() {
-		Pesquisa p1 = new Pesquisa();
-		p1.setId(1);
-//		p1.getCandidatos();
-//		p1.getPesquisa();
-		p1.setVotos(100);
-		
-		Pesquisa p2 = new Pesquisa();
-		p2.setId(2);
-//		p2.setCandidatos(p2.getCandidatos());
-//		p2.setPesquisa(p2.getPesquisa());
-		p2.setVotos(150);
-		
-		PesquisaController controller = new PesquisaController();
-		try {
-			controller.salvar(p1);
-			controller.salvar(p2);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		for (Pesquisa p : controller.listar()) {
-			System.out.println(p.toString());
-		}
-	}
+
+	
+//	public static void testeCandidato() {
+//		Candidato c1 = new Candidato();
+//		c1.setId(1);
+//		c1.setNome("Bolsonaro");
+//		c1.setPartido("PSL");
+//		c1.setFichaLimpa("Sim");
+//		
+//		Candidato c2 = new Candidato();
+//		c2.setId(2);
+//		c2.setNome("Lula");
+//		c2.setPartido("PT");
+//		c2.setFichaLimpa("Não");
+//		
+//		CandidatoController controller = new CandidatoController();
+//		try {
+//			controller.salvar(c1);
+//			controller.salvar(c2);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		for (Candidato c : controller.listar()) {
+//			System.out.println(c.toString());
+//		}
+//		
+//		
+//	}
+	// comentario teste
+//	public static void testeTipoPesquisa() {
+//		TipoPesquisa tp1 = new TipoPesquisa();
+//		tp1.setId(15);
+//		tp1.setInstituto("Data Folha");
+//		tp1.setDataPesquisa(LocalDate.now());
+//		tp1.setLocal("Florianópolis");
+//		tp1.setMediaIdade(45.5);
+//		tp1.setTipoPesquisa("Prefeito");
+//		tp1.setDescricao("Pesquisa para prefeito");
+//		
+//		TipoPesquisa tp2 = new TipoPesquisa();
+//		tp2.setId(25);
+//		tp2.setInstituto("Fake News");
+//		tp2.setDataPesquisa(LocalDate.now());
+//		tp2.setLocal("Rio de Janeiro");
+//		tp2.setMediaIdade(32.7);
+//		tp2.setTipoPesquisa("Presidente");
+//		tp2.setDescricao("Pesquisa para presidente");
+//
+//		
+//		TipoPesquisaController controller = new TipoPesquisaController();
+//		try {
+//			controller.salvar(tp1);
+//			controller.salvar(tp2);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		for (TipoPesquisa tp : controller.listar()) {
+//			System.out.println(tp.toString());
+//		}
+//	}
+	
+//	public static void testePesquisa() {
+//		Pesquisa p1 = new Pesquisa();
+//		p1.setId(1);
+////		p1.getCandidatos();
+////		p1.getPesquisa();
+//		p1.setVotos(100);
+//		
+//		Pesquisa p2 = new Pesquisa();
+//		p2.setId(2);
+////		p2.setCandidatos(p2.getCandidatos());
+////		p2.setPesquisa(p2.getPesquisa());
+//		p2.setVotos(150);
+//		
+//		PesquisaController controller = new PesquisaController();
+//		try {
+//			controller.salvar(p1);
+//			controller.salvar(p2);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		for (Pesquisa p : controller.listar()) {
+//			System.out.println(p.toString());
+//		}
+//	}
 
 }
--- src/dao/CandidatoDao.java
+++ src/dao/CandidatoDao.java
@@ -1,14 +1,21 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Candidato;
+import util.ConnectionUtil;
 
 public class CandidatoDao {
 
 	private static CandidatoDao instance;
 	private List<Candidato> listaCandidatos = new ArrayList<>();
+	private Connection con = ConnectionUtil.getConnection();
 	
 	/*
 	 * Singleton - Design Pattern
@@ -22,18 +29,66 @@ public class CandidatoDao {
 	}
 	
 	public void salvar(Candidato candidato) {
-		listaCandidatos.add(candidato);
+		try {
+//			Connection conexao = ConnectionUtil.getConnection();
+			String sql = "insert into candidato (idcandidato, nome, ficha_limpa, partido) values (?, ?, ?, ?)";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, candidato.getId());
+			pstmt.setString(2, candidato.getNome());
+			pstmt.setString(3, candidato.getFichaLimpa());
+			pstmt.setString(4, candidato.getPartido());
+			pstmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+
 	
 	public void atualizar(Candidato candidato) {
-		listaCandidatos.set(candidato.getId(), candidato);
+		try {
+//			Connection conexao = ConnectionUtil.getConnection();
+			String sql = "update candidato set nome = ?, ficha_limpa = ?, partido = ? where id = ?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, candidato.getNome());
+			pstmt.setString(2, candidato.getFichaLimpa());
+			pstmt.setString(3, candidato.getPartido());
+			pstmt.setInt(3, candidato.getId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void excluir(int idCandidato) {
-		listaCandidatos.remove(idCandidato);
+		try {
+			String sql = "delete from candidato where id = ?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, idCandidato);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public List<Candidato> listar(){
+		List<Candidato> listaCandidatos = new ArrayList<>();
+		try {
+//			Connection conexao = ConnectionUtil.getConnection();
+			String sql = "select * from candidato";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				Candidato c = new Candidato();
+				c.setId(rs.getInt("id"));
+				c.setNome(rs.getString("nome"));
+				c.setFichaLimpa(rs.getString("ficha_limpa"));
+				c.setPartido(rs.getString("partido"));
+				listaCandidatos.add(c);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return listaCandidatos;
 	}
 	
